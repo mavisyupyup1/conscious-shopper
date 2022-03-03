94df1b333976efc649da900d75d6217bc1909a37
mavisyupyup1 <mavisyupyup1@github.com>
2022-03-03 07:40:13 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+//import ApolloServer
+const {ApolloServer}=require('apollo-server-express')
+
+//import our typeDefs and resolvers
+const {typeDefs, resolvers} =require('./schemas')
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+const path= require('path')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer =async()=>{
+  //create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  //integrate our Apollo server with the express application as middleware
+  server.applyMiddleware({app});
+
+  //log where we can go to test out GQL API
+  console.log(`Use graphQL at http://localhost:${PORT}${server.graphqlPath}`)
+};
+
+//initialize the Apollo Server
+startServer();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'../client/build')))
+}
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname,'../client/build/index.html'))
+})
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
