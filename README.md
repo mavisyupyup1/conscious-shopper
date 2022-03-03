c1ccbfc8309fbfae750ee8e0c887cefa81bf63a7
mavisyupyup1 <mavisyupyup1@github.com>
2022-03-03 07:48:22 -0500

    added deploed link

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project-3-group-d
+
+## Deployed Link
+https://enigmatic-basin-52191.herokuapp.com/ 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/project-3', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
