5eccf5426a0e401e93ae53a67e686b28eef13f33
alvaroormeno <alvaro@ormeno.org>
2022-03-10 20:59:56 -0500

    top 5 cards finished, only missing image

--- client/src/components/Top5Card/index.js
+++ client/src/components/Top5Card/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
-import { Card, Badge, Button } from 'react-bootstrap';
+import { Card, Badge, Button, Col } from 'react-bootstrap';
 
 var Top5Card = function ({ data, setOrdered }) {
 
   return (
 
-    <Card className="h-100 shadow-sm bg-white rounded">
-      {/* <Card.Img variant="top" src={data.image} /> */}
-      <Card.Body className="d-flex flex-column">
-        <div className="d-flex mb-2 justify-content-between">
-          <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
-          <Badge pill className="mb-1" variant="warning">
-            £{data.price}
-          </Badge>
-        </div>
-        <Card.Text className="text-secondary">{data.desc}</Card.Text>
-        <Button
-          onClick=""
-          className="mt-auto font-weight-bold"
-          variant="success"
-          block
-        >
-          Order Pizza 🍕
-        </Button>
-      </Card.Body>
-    </Card>
+    
+      <Col>
+
+        <Card className="h-100 shadow-sm bg-white rounded mt-2">
+        {/* <Card.Img variant="top" src={data.image} /> */}
+        <Card.Body className="d-flex flex-column">
+            <div className="d-flex mb-2 justify-content-between">
+            <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
+            <Badge pill className="mb-1" variant="warning">
+            🔴 {data.type}
+            </Badge>
+            </div>
+            <Card.Text className="text-secondary">{data.desc}</Card.Text>
+            <Button
+            onClick=""
+            className="mt-auto font-weight-bold"
+            variant="success"
+            block
+            >
+            More Info  🔍 
+            </Button>
+        </Card.Body>
+        </Card>
+
+      </Col>
+
+    
   );
 }
 
